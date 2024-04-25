0adb2415db544e06a497c7108d828b23b9f2158b
mina-william-23 <capeandroid50@gmail.com>
2024-04-25 11:40:01 +0200

    initial commit

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,18 @@
+# Pascal's Triangle is a mathematical construct that follows a simple recursive formula.
+
+
+# Create a function that will return a 2D list representation of Pascal's Triangle up to the nth level.
+def generate_pascals_triangle(num_rows):
+    """Generate Pascal's Triangle up to the nth level."""
+    triangle = []
+
+    for row_num in range(num_rows):
+        row = [None] * (row_num + 1)
+        row[0], row[-1] = 1, 1
+
+        for j in range(1, len(row) - 1):
+            row[j] = triangle[row_num - 1][j - 1] + triangle[row_num - 1][j]
+
+        triangle.append(row)
+
+    return triangle
